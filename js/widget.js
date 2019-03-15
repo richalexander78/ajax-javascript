4ea5d52bf7046581b860d0d0b22cfe46eb18d4ac
Rich <richalexander78@users.noreply.github.com>
2019-03-15 14:05:29 -0400

    Add files via upload

---
+++ js/widget.js
@@ -0,0 +1,35 @@
+//create xmlhttp request object
+const xhr = new XMLHttpRequest();
+
+ // create callback function to do something with response
+xhr.onreadystatechange = function () {
+	if(xhr.readyState === 4) {
+		if(xhr.status === 200) {
+			const employees = JSON.parse(xhr.responseText);
+			
+			let statusHTML = '<ul class="bulleted">';
+			
+			for (let i = 0; i < employees.length; i ++) {
+			if (employees[i].inoffice === true) {
+			statusHTML += '<li class="in">';
+			} else {
+			statusHTML += '<li class="out">';  
+			}
+			statusHTML += employees[i].name;
+			statusHTML += '</li>';
+			}
+			
+			statusHTML += '</ul>'; 
+			
+			document.querySelector('#employeeList').innerHTML = statusHTML;
+		} else {
+			alert(xhr.statusText);
+		}
+	}
+};
+
+//open a request (prepare browser for sending request)
+xhr.open('GET', 'data/employees.json');
+
+//send the request
+xhr.send();
